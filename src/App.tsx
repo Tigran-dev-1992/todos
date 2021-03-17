aa1f30a7c4227dc4277fbaec954181ccd5180d7a
Tigran-dev-1992 <tikokarap@mail.ru>
2021-03-17 20:36:48 +0400

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {HashRouter, Route, Switch } from 'react-router-dom'
 import AboutPage from './Components/AboutPage/AboutPage';
 import HomePage from './Components/Hompage/HomePage';
 import Navbar from './Components/Navbar/Navbar';
@@ -8,19 +8,23 @@ import { NotesState } from './Components/Hompage/Notes/NotesState';
 
 function App() {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
+    <HashRouter>
       <NotesState>
         <AlertState>
           <Navbar />
-          <div className="app pt-5">
-            <div className="container pt-4">
-              <Route path="/" component={HomePage} exact />
-              <Route path="/about" exact component={AboutPage} />
+          <Switch>
+            <div className="app pt-5">
+              <div className="container pt-4">
+                <Route path="/" component={HomePage} exact />
+                <Route path="/about" exact component={AboutPage} />
+              </div>
             </div>
-          </div>
+          </Switch>
         </AlertState>
       </NotesState>
-    </BrowserRouter >
+    </HashRouter>
+    // </BrowserRouter>
   );
 }
 
